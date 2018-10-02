3dfed543ccd553cf6b79119682ec74c363d16c09
Sky Young <OstlerDev@users.noreply.github.com>
2018-10-02 13:47:08 -0600

    Check if sale price is >= the sell order price

--- src/SellOrder.js
+++ src/SellOrder.js
@@ -4,6 +4,13 @@ import Order from './Order'
 class SellOrder extends Order {
 	constructor(opts){
 		super(opts)
-
+	}
+	addSale(sale){
+		assert(sale.price >= this.getPrice(), "Sale Price must be > or = to the Sell Order price!")
+		
+		// Call the parent method
+		Order.prototype.addSale.call(this, sale)
 	}
 }
+
+export default SellOrder

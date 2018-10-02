aadbd1e1d446a10c926efd1e712a8c4ee58b45e6
Sky Young <OstlerDev@users.noreply.github.com>
2018-10-02 14:05:29 -0600

    Enforce price = number & qty = integer on addSale

--- src/Order.js
+++ src/Order.js
@@ -10,7 +10,15 @@ class Order {
 
 		this.sales = []
 	}
+	/**
+	 * Add a sale to the Order
+	 * @param {Object} sale - The Sale to process
+	 * @param {Number} sale.price - The purchase price of the sale
+	 * @param {Integer} sale.quantity - The purchase quantity of the sale
+	 */
 	addSale(sale){
+		assert(!Number.isNaN(sale.price), "Sale Price must be a Number!")
+		assert(Number.isInteger(sale.quantity), "Sale Quantity must be an Integer!")
 		assert(sale.quantity <= this.getQuantity(), "Sale Quantity must be < or = to the Quantity available on the Order!")
 
 		this.sales.push(sale)
